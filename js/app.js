aa685e0c314b7062e78332fcc1c5cbd10d13ab48
Sergio <sergio.eneide@protonmail.com>
2021-07-23 19:00:51 +0100

    13th commit

--- js/app.js
+++ js/app.js
@@ -1,41 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
     const newItemform = document.querySelector('#new-item-form');
     newItemform.addEventListener('submit', handleNewItemFormSubmit);
-
+  
     const deleteAllButton = document.querySelector('#delete-all');
     deleteAllButton.addEventListener('click', handleDeleteAllClick);
   })
-
+  
   const handleNewItemFormSubmit = function (event) {
     event.preventDefault();
   
-    const genreListItem = createReadingListItem(event.target);
-    const genreList = document.querySelector('#genre-list');
-    genreList.appendChild(genreListItem);
+    const gameListItem = createGameListItem(event.target);
+    const gameList = document.querySelector('#game-list');
+    gameList.appendChild(gameListItem);
   
     event.target.reset();
-  }  
-
-  const createGenreListItem = function (form) {
-    const genreListItem = document.createElement('li');
-    genreListItem.classList.add('genre-list-item');
-
-    const gameTitle = document.createElement('h2');
-  gameTitle.textContent = form.gameTitle.value;
-  genreListItem.appendChild(title);
-
-  const publisher = document.createElement('h3');
-  publisher.textContent = form.publisher.value;
-  genreListItem.appendChild(publisher);
-
-  const genre = document.createElement('p');
-  genre.textContent = form.genre.value;
-  genreListItem.appendChild(genre);
-
-  return genreListItem;
-}
-
-const handleDeleteAllClick = function (event) {
-  const genreList = document.querySelector('#genre-list');
-  genreList.innerHTML = '';
-}
+  }
+  
+  const createGameListItem = function (form) {
+    const gameListItem = document.createElement('li');
+    gameListItem.classList.add('game-list-item');
+  
+    const game = document.createElement('h2');
+    game.textContent = form.game.value;
+    gameListItem.appendChild(game);
+  
+    const publisher = document.createElement('h3');
+    publisher.textContent = form.publisher.value;
+    gameListItem.appendChild(publisher);
+  
+    const genre = document.createElement('p');
+    genre.textContent = form.genre.value;
+    gameListItem.appendChild(genre);
+  
+    return gameListItem;
+  }
+  
+  const handleDeleteAllClick = function (_event) {
+    const gameList = document.querySelector('#game-list');
+    gameList.innerHTML = '';
+  }
